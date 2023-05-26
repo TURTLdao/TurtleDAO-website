2a1cc69166fd5988e996cfdb9d9cf7a67db7b97e
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-26 02:03:20 +0100

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -18,7 +18,8 @@ const Hero = () => {
                 href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
                 target="_blank"
                 rel="noopener"
-                className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
+                className="px-8 py-4 text-lg font-medium text-center text-white  rounded-md "
+                style={{ background: '#4CAF50' }}>
                 Launch App
               </a>
             </div>
